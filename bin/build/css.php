5628b50baf1a662ea69d6973e11002344f978796
Mike Thorn <mthorn@bahamut.(none)>
2013-05-24 09:10:31 -0400

    adding font-awesome to build system

--- bin/build/css.php
+++ bin/build/css.php
@@ -18,9 +18,27 @@ if(file_exists(__DIR__.'/../../www/media/less/customizations.less'))
 }
 
 
-file_put_contents(__DIR__.'/../../www/media/combined.css',$less->compile($to_compile));
+$fontawesome = file_get_contents(__DIR__.'/../../lib/FontAwesome/build/assets/font-awesome/css/font-awesome.css');
+$fontawesome_min = file_get_contents(__DIR__.'/../../lib/FontAwesome/build/assets/font-awesome/css/font-awesome.min.css');
+$fontawesome = str_replace('../font/','fonts/',$fontawesome);
+$fontawesome_min = str_replace('../font/','fonts/',$fontawesome_min);
+
+file_put_contents(
+	__DIR__.'/../../www/media/combined.css',
+	$less->compile($to_compile)."\n".$fontawesome
+);
 $less->setFormatter("compressed");
-file_put_contents(__DIR__.'/../../www/media/combined.min.css',$less->compile($to_compile));
+file_put_contents(
+	__DIR__.'/../../www/media/combined.min.css',
+	$less->compile($to_compile)."\n".$fontawesome_min
+);
+
+copy(__DIR__.'/../../lib/FontAwesome/build/assets/font-awesome/font/fontawesome-webfont.eot',__DIR__.'/../../www/media/fonts/fontawesome-webfont.eot');
+copy(__DIR__.'/../../lib/FontAwesome/build/assets/font-awesome/font/fontawesome-webfont.svg',__DIR__.'/../../www/media/fonts/fontawesome-webfont.svg');
+copy(__DIR__.'/../../lib/FontAwesome/build/assets/font-awesome/font/fontawesome-webfont.ttf',__DIR__.'/../../www/media/fonts/fontawesome-webfont.ttf');
+copy(__DIR__.'/../../lib/FontAwesome/build/assets/font-awesome/font/fontawesome-webfont.woff',__DIR__.'/../../www/media/fonts/fontawesome-webfont.woff');
+copy(__DIR__.'/../../lib/FontAwesome/build/assets/font-awesome/font/FontAwesome.otf',__DIR__.'/../../www/media/fonts/FontAwesome.otf');
+
 
 echo("     COMPLETE!\n");
 ?>
