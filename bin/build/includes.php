d24a4b72051127bc50b6697e01324622bcaf12ba
Mike Thorn <mthorn@bahamut.(none)>
2013-07-02 14:16:58 -0400

    updating build process

--- bin/build/includes.php
+++ bin/build/includes.php
@@ -2,9 +2,9 @@
 <?php
 echo("Starting build process...\n");
 echo("---------------------------------\n");
-echo shell_exec('php -f bin/build/css.php');
-echo shell_exec('php -f bin/build/js.php');
-echo shell_exec('php -f bin/build/models.php');
+echo shell_exec('php -f bin/build/includes-css.php');
+echo shell_exec('php -f bin/build/includes-js.php');
+echo shell_exec('php -f bin/build/includes-models.php');
 echo("---------------------------------\n");
 echo("Build complete!\n");
 ?>
