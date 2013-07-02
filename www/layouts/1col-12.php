b7b9dfa6cd8be1efc4ee64de5afbaa4f6e408311
Mike Thorn <mthorn@bahamut.(none)>
2013-07-01 22:24:12 -0400

    adding 1 col layout

---
+++ www/layouts/1col-12.php
@@ -0,0 +1,33 @@
+<?php
+
+global $page,$config;
+
+$page = bsc::page(jvc::get_response('title'))
+	->javascript($__bsc['initial_js'])
+	->description(jvc::get_response('description'))
+	->description(jvc::get_response('keywords'))
+	->description(jvc::get_response('author'))
+	->css($config['bsc']['libs']['css'])
+	->head_js($config['bsc']['libs']['head_js'])
+	->foot_js($config['bsc']['libs']['foot_js'])
+	->onload('csn.init();');
+
+$page->add(
+	jvc::get_response('top'),
+	bsc::row()->add(
+		bsc::div()->span(12)->add(
+			bsc::navbar()->id('header')->add(jvc::get_response('header'))
+		)
+	),
+	bsc::row()->add(
+		bsc::div()->span(12)->id('center')->add(jvc::get_response('center'))
+	),
+	bsc::row()->add(
+		bsc::div()->span(12)->add(
+			bsc::navbar()->id('footer')->add(jvc::get_response('footer'))
+		)
+	),
+	jvc::get_response('bottom')
+);
+
+?>
